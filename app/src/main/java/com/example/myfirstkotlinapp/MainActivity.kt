002fdd66ca1cfb84869b00c8c8547c809d252846
Mehdi Khalfallah <contact.mehdi.khalfallah@gmail.com>
2022-04-09 15:56:18 +0100

    counter done

--- app/src/main/java/com/example/myfirstkotlinapp/MainActivity.kt
+++ app/src/main/java/com/example/myfirstkotlinapp/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.myfirstkotlinapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnclickme = findViewById<Button>(R.id.ClickMe)
+        val text = findViewById<TextView>(R.id.textView2)
+        var count = 0
+        btnclickme.setOnClickListener {
+
+            text.text = count.toString()
+            count += 1
+        }
     }
 }
